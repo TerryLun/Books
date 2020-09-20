18157ca50eb2b7120d14024930413203d0bb3871
TerryLun <tianweilun@yahoo.com>
2020-09-20 03:21:07 -0700

    Removed dead code.

--- src/book/io/CustomerReader.java
+++ src/book/io/CustomerReader.java
@@ -16,8 +16,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,6 +23,7 @@ import org.apache.logging.log4j.Logger;
 import book.ApplicationException;
 import book.data.Customer;
 import book.db.CustomerDao;
+import book.data.util.Validator;
 
 /**
  * @author Terry Tianwei Lun
@@ -136,43 +135,5 @@ public class CustomerReader extends Reader {
 		}
 
 		return customer;
-	}
-
-	private static class Validator {
-
-		private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-		private static final String YYYYMMDD_PATTERN = "(20\\d{2})(\\d{2})(\\d{2})"; // valid for years 2000-2099
-
-		private static Pattern emailPattern;
-		private static Pattern yyyymmddPattern;
-
-		private Validator() {
-		}
-
-		/**
-		 * Validate an email string.
-		 * 
-		 * @param email
-		 *            the email string.
-		 * @return true if the email address is valid, false otherwise.
-		 */
-		public static boolean validateEmail(final String email) {
-			if (emailPattern == null) {
-				emailPattern = Pattern.compile(EMAIL_PATTERN);
-			}
-
-			Matcher matcher = emailPattern.matcher(email);
-			return matcher.matches();
-		}
-
-		public static boolean validateJoinedDate(String yyyymmdd) {
-			if (yyyymmddPattern == null) {
-				yyyymmddPattern = Pattern.compile(YYYYMMDD_PATTERN);
-			}
-
-			Matcher matcher = yyyymmddPattern.matcher(yyyymmdd);
-			return matcher.matches();
-		}
-
 	}
 }
